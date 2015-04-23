ff587b41b67177a6e30af246ca346e73503cef0e
Joao Coutinho <me@joaoubaldo.com>
2015-04-23 18:28:12 +0100

    praticamente todo o js reescrito e site implementado com angularjs, openlayers3 e bootstrap. routing a funcionar

--- jcoutinho/routing_com_opcoes/js/main.js
+++ jcoutinho/routing_com_opcoes/js/main.js
@@ -1,16 +1,10 @@
 window.app = {};
 var app = window.app;
+var ngapp = angular.module('sigApp', []);
 
 
 var maxExtent = ol.proj.transformExtent([-8.870560,41.029011,-8.323991,40.438993], 'EPSG:4326', 'EPSG:3857');
-var params, viewparams, startNode, endNode;
-var coordInicial, coordDestino;
-var cDestino;
 
-var startNode = new ol.Feature();
-var endNode = new ol.Feature();
-
-var routingLayer;
 
 var layers = [
     {
@@ -81,6 +75,7 @@ var startStyle = [
 	})
 ];
 
+
 var endStyle = [
 	new ol.style.Style({
 		image: new ol.style.Icon({
@@ -106,45 +101,75 @@ var endStyle = [
 
 
 
-$(document).ready(function () {
-
-    $('#limpar').click(function(ev) {
-          startNode.setGeometry(null);
-          endNode.setGeometry(null);
-          // Remover layer "resultado".
-          map.removeLayer(resultado);
-          cDestino=null;
-          coordDestino = null;
+ngapp.controller('mainController', function($scope) {
+    $scope.algoritms = layers;
+    $scope.algoritm = null;
+    $scope.view = new ol.View({
+	    projection: 'EPSG:3857',
+	    center:ol.proj.transform([-8.651697, 40.641121], 'EPSG:4326', 'EPSG:3857'),
+	    extent:maxExtent,
+		zoom:13,
+		minZoom:13,
+		maxZoom:17
+    });
+    $scope.map = new ol.Map({
+        //interactions: ol.interaction.defaults().extend([new app.Drag()]),
+	    layers:[baseLayers[0]],
+        target:'mapa',
+        renderer:'canvas',
+	    view:$scope.view
     });
 
-    function findClosestNode(x, y, cb) {
-        var json_url = "./scripts/pontoMaisProximo.php?";
-	    json_url += "x=" + x;
-	    json_url += "&y=" + y;
-	    $.ajax({
-		    url: json_url,
-		    async: true,
-		    success: function(data){
-			    var data = jQuery.parseJSON(data);
-                cb(parseFloat(data[0].x), parseFloat(data[0].y));
-		    }
-	    });
+    $scope.startNode = new ol.Feature();
+    $scope.endNode = new ol.Feature();
+
+
+    $scope.setAlgoritm = function(a) {
+        $scope.algoritm = a;
+        console.log("Algoritm set", a, $scope.startNode, $scope.endNode);
+
+        if ($scope.startNode.getGeometry() != null && $scope.endNode.getGeometry() != null) {
+            var coordInicial = transform($scope.startNode.getGeometry().getCoordinates());
+            var coordDestino = transform($scope.endNode.getGeometry().getCoordinates());
+            $scope.addResultado(coordInicial,coordDestino,$scope.algoritm.key, $scope.algoritm.format);
+        }
     };
 
-    function addResultado (coordInicial,coordDestino) {
+    $scope.clearRouting = function() {
+          $scope.startNode.setGeometry(null);
+          $scope.endNode.setGeometry(null);
+          // Remover layer "resultado".
+          $scope.map.removeLayer(resultado);
+          $scope.coordDestino = null;
+    };
+
+    $scope.findClosestNode = function(x, y, cb) {
+        var json_url = "./scripts/pontoMaisProximo.php?";
+        json_url += "x=" + x;
+        json_url += "&y=" + y;
+        $.ajax({
+	        url: json_url,
+	        async: true,
+	        success: function(data){
+		        var data = jQuery.parseJSON(data);
+                cb(parseFloat(data[0].x), parseFloat(data[0].y));
+	        }
+        });
+    };
+
+
+    $scope.addResultado = function (coordInicial,coordDestino, layer, format) {
         viewparams = [
           'x1:' + coordInicial[0], 'y1:' + coordInicial[1],
           'x2:' + coordDestino[0], 'y2:' + coordDestino[1]
         ];
         params = {
-            LAYERS: routingLayer.key,
-            FORMAT: routingLayer.format
+            LAYERS: layer,
+            FORMAT: format
         };
-        console.log(params);
         params.viewparams = viewparams.join(';');
 
-        $("#selectpicker").val()
-
+        console.log(params);
         resultado = new ol.layer.Image({
           source: new ol.source.ImageWMS({
             name:'resultado',
@@ -154,104 +179,50 @@ $(document).ready(function () {
         });   
         //var extent = layerVetorial.getSource().getExtent();
         //map.getView().fitExtent(extent, map.getSize());
-        map.addLayer(resultado);
-        console.log(resultado);
-    };
-
-    function setRoutingLayer(key) {
-        routingLayer = null;
-
-        $(layers).each(function(idx, el) {
-            if (el.key == key) {
-                routingLayer = el;
-            }
-        });
-
-        $('#selectpicker button').text(routingLayer.name);
+       $scope.map.addLayer(resultado);
     };
 
 
-    $(layers).each(function(idx, el) {
-        li = $('<li></li>').attr('role', 'presentation');
-        a = $('<a></a>').attr('role', 'menuitem').attr('tabindex', '-1').attr('href', '#').val(el.key).text(el.name);
-        li.click(function(e) {
-            setRoutingLayer(el.key);
-        });
-        li.append(a);
-        $('#selectpicker .dropdown-menu').append(li);
-    });
-    setRoutingLayer(layers[0].key);
+    // main()
 
-    view= new ol.View({
-	    projection: 'EPSG:3857',
-	    center:ol.proj.transform([-8.651697, 40.641121], 'EPSG:4326', 'EPSG:3857'),
-	    extent:maxExtent,
-		zoom:13,
-		minZoom:13,
-		maxZoom:17
-    });
-
-    map = new ol.Map({
-        //interactions: ol.interaction.defaults().extend([new app.Drag()]),
-	    layers:[baseLayers[0]],
-        target:'mapa',
-        renderer:'canvas',
-	    view:view
-    });
-
-    startNode.setStyle(startStyle);
-    endNode.setStyle(endStyle);
+    $scope.startNode.setStyle(startStyle);
+    $scope.endNode.setStyle(endStyle);
 
     var layerVetorial = new ol.layer.Vector({
 	    source: new ol.source.Vector({
-      		features: [startNode, endNode]
+      		features: [$scope.startNode, $scope.endNode]
       	})
     });
 
-    map.addLayer(layerVetorial);
+    $scope.map.addLayer(layerVetorial);
 
-    // Função de transformação para converter coordenadas de 
-    // EPSG:3857 para EPSG:4326.
     var transform = ol.proj.getTransform('EPSG:3857', 'EPSG:4326');
 
-    map.on('click', function(event) {
+
+    $scope.map.on('click', function(event) {
         var x = event.coordinate[0], y = event.coordinate[1];
 
-	    if (startNode.getGeometry() == null) {
+	    if ($scope.startNode.getGeometry() == null) {
 		    var json_url = "./scripts/pontoMaisProximo.php?";
 		    json_url += "x=" + x;
 		    json_url += "&y=" + y;
             console.log(json_url);
         	$.ajax({url: json_url, async:false, success: function(data){
-				    var data = jQuery.parseJSON(data);
-				    startNode.setGeometry(new ol.geom.Point([data[0].x, data[0].y]));
-				    console.log(cDestino);
-
+			    var data = jQuery.parseJSON(data);
+			    $scope.startNode.setGeometry(new ol.geom.Point([data[0].x, data[0].y]));
 		    }});
-		    coordInicial = transform(startNode.getGeometry().getCoordinates());
+		    $scope.coordInicial = transform($scope.startNode.getGeometry().getCoordinates());
 
-		    document.getElementById("selectpicker").disabled = true;
-		    console.log(cDestino);
-		    if( typeof cDestino === 'undefined' || cDestino === null ) {$('.typeahead').disableSelection();} else{console.log(cDestino);addResultado(coordInicial,cDestino);document.getElementById("selectpicker").disabled = false;}
-		
-	    } else if (endNode.getGeometry() == null) {
+	    } else if ($scope.endNode.getGeometry() == null) {
 
-            //Segundo click.
-
-            findClosestNode(x,y, function(x,y) {
-                endNode.setGeometry(new ol.geom.Point([x, y]));
-                // Transformar as coordenadas da projeção do mapa (EPSG:3857)
-                // para a projeção dos dados na base de dados (EPSG:4326).
-                var coordInicial = transform(startNode.getGeometry().getCoordinates());
-	            var coordDestino = transform(endNode.getGeometry().getCoordinates());
-	            addResultado(coordInicial,coordDestino);
-	            document.getElementById("selectpicker").disabled = false;
+             $scope.findClosestNode(x,y, function(x,y) {
+                $scope.endNode.setGeometry(new ol.geom.Point([x, y]));
+                $scope.coordDestino = transform($scope.endNode.getGeometry().getCoordinates());
+	            $scope.addResultado($scope.coordInicial, $scope.coordDestino, $scope.algoritm.key, $scope.algoritm.format);
             });
 
         }
     });
 
-
-    $('#selectpicker').change(function(){
-    });
 });
+
